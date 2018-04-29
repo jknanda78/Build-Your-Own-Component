7616e1b9008edbc23d8d2920fb85e6e2e54f6d73
jnanda <jyoti.kaustuv.nanda@gmail.com>
2018-04-28 22:46:46 -0700

    remove console logs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { h } from 'preact';
 import AddCard from './js/components/add-card';
-import { VaultDesignInputField, AtomDesignInputField } from '@jsfoobar/design-patterns';
+import { VaultDesignInputField } from '@jsfoobar/design-patterns';
 
 const App = () => (
 	<AddCard DesignPattern={VaultDesignInputField}  />
